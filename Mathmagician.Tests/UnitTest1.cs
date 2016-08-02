e25b8a3cf9645c26d6d911ee4cd6a3b2e2455322
Andi DeVois <adevois@gmail.com>
2016-08-01 19:03:58 -0700

    unittest project setup and first test

--- Mathmagician.Tests/UnitTest1.cs
+++ Mathmagician.Tests/UnitTest1.cs
@@ -6,9 +6,29 @@ namespace Mathmagician.Tests
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        public void ThisIsNotATest()
         {
+
+        }
+
+        [TestMethod]
+        public void TestThatThisIsTrue()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void ThisShouldFail()
+        {
+            //Arrange
+            //Act
+            //Assert
+            Assert.Fail();
         }
     }
 }
