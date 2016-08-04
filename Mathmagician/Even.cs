78d915cf85b93cfbf41d5b9add54db3b7cadb85b
JZorvan <jzorvan@gmail.com>
2016-08-03 19:24:53 -0700

    First draft Iteration for Even nums

---
+++ Mathmagician/Even.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class Even
+    {
+        public List<int> returnList = new List<int>();
+
+        public int workingNum = 0;
+
+        public void Iterate(int userNum)
+        {
+            for (int i = 0; i < userNum; i++)
+            {
+                returnList.Add(workingNum);
+                workingNum = workingNum + 2;
+            }
+
+            Console.WriteLine("Here they are:  ");
+            returnList.ForEach(Console.Write);
+
+        }
+
+    }
+}
