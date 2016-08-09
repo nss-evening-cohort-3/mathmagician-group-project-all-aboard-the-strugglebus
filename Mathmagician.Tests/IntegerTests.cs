19d411cf3bd79408d15cf3e3630de775d9fe7c9b
Andi DeVois <adevois@gmail.com>
2016-08-08 17:50:52 -0700

    integer implementation and tests

--- Mathmagician.Tests/IntegerTests.cs
+++ Mathmagician.Tests/IntegerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Mathmagician.Tests
 {
@@ -18,5 +19,47 @@ namespace Mathmagician.Tests
             //Assert
             Assert.IsNotNull(my_int);
         }
+
+        [TestMethod]
+        public void IntegerFirstInstance()
+        {
+            //Arrange
+            Integer my_int = new Integer();
+
+            //Act
+            int NumberZero = my_int.GetFirstNumber();
+
+
+            //Assert
+            Assert.AreEqual(0, NumberZero);
+        }
+
+        [TestMethod]
+        public void IntegerGetNext()
+        {
+            //Arrange
+            Integer my_int = new Integer();
+
+            //Act
+            int NextNumber = my_int.GetNext(42);
+
+            //Assert
+            Assert.AreEqual(43, NextNumber);
+        }
+
+        [TestMethod]
+        public void MakeAList()
+        {
+            //Arrange
+            Integer my_int = new Integer();
+
+            //Act
+            List<int> NumberList = my_int.GetList(10);
+
+            //Assert
+            CollectionAssert.AreEqual(new List<int> { 1, 2, 3, 4, 5, 6,7, 8, 9, 10 }, NumberList);
+        }
+
+
     }
 }
--- Mathmagician/Integer.cs
+++ Mathmagician/Integer.cs
@@ -8,5 +8,36 @@ namespace Mathmagician
 {
     public class Integer
     {
+        public List<int> integers = new List<int>();
+
+        public void Iteration(int input)
+        {
+
+            for (int i = 0; i < input; i++)
+            {
+                integers.Add(i);
+
+            };
+        }
+          
+
+        public int GetFirstNumber()
+        {
+            return 0;
+        }
+
+        public int GetNext(int v)
+        {
+            return v+1;
+        }
+
+        public List<int> GetList(int end)
+        {
+            List<int> list = new List<int>();
+            for (int i = 0; i < end; i++)
+                list.Add(i + 1);
+            return list;
+        }
+
     }
 }
