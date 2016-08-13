589de9d34ebdca2bfd1a7ced5b8e5d64e2ab3c34
Andi DeVois <adevois@gmail.com>
2016-08-13 08:49:30 -0700

    fibonacci sequence and tests working

---
+++ Mathmagician/Fibonacci.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class Fibonacci
+    {
+
+        public List<int> Sequence(int userNum)
+        {
+            if (userNum == 1)
+            {
+                return new List<int> { 0 };
+            }
+            if (userNum == 2)
+            {
+                return new List<int> { 0, 1 };
+            }
+            List<int> FibonacciSequence = new List<int> { 0, 1 };
+            int prevNum = 0;
+            int currentNum = 1;            
+
+            for (int i = 2; i < userNum; i ++)
+            {
+                int sum = currentNum + prevNum;
+                prevNum = currentNum;
+                currentNum = sum;
+               
+                FibonacciSequence.Add(currentNum);
+  
+
+            }
+
+            return FibonacciSequence;
+
+
+        }
+    }
+}
