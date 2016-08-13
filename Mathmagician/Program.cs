d9d17994b50bcf2bff9ea626ab2d73a0d0d9e4d2
JZorvan <jzorvan@gmail.com>
2016-08-13 10:05:14 -0700

    Program will work with Prime, Fibonacci, Integer.  Cleaned up and commented

--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -9,6 +9,7 @@ namespace Mathmagician
     class Program
     {
         // Functions Section
+
         // This function capitalizes the first character of the user's selected type
         public static string FirstCharToUpper(string input)
         {
@@ -18,12 +19,15 @@ namespace Mathmagician
         static void Main(string[] args)
         {
             // Variables Section
-            String prompt = "> ";  // Pretty little prompt symbol for the user/loser
-            String userTypeProper = "";
-            // A list of the possible choices in lowercase to loop through for (I really need) validation
+
+            String prompt = "> ";  // Prompt symbol for the user
+            String userTypeProper = "";  // Will hold the capitalized user selection
+            // A list of the possible choices in lowercase to loop through for validation
             List<string> Choices = new List<string>() { "integer", "prime", "fibonacci", "even", "odd" };
+            List<int> ReturnList = new List<int>();  // List to hold the iteration
 
             // Type Section
+
             // Asks for the type of number input from user
             Console.WriteLine("What kind of number sequence would you like me to print?");
             Console.WriteLine("Options I know: Integer, Prime, Fibonacci, Even, Odd");
@@ -42,22 +46,61 @@ namespace Mathmagician
             else
             {
                 // If user did not choose an accepted option:
-                Console.WriteLine("Please enter one of the following choices: Integer, Prime, Fibonacci, Even, Odd");
+                Console.WriteLine("Apparently you can't read, so I will show you can example with Integers.");
+                userTypeProper = "Integer";
             }
-            // Fuck it, I'm tired of this shit so I'm taking a break on figuring out how to run that bullshit again and moving on to the #s.
 
-            // Fuck yeah, Number of Iterations Section!
+            // Number of Iterations Section
+
             // Asks for what length of sequence the user would like to see returned
             Console.WriteLine("Next.. How long would you like this sequence of " + userTypeProper + " numbers to be?");
             Console.Write(prompt);
-            int userNum = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Spiffers! I'm going to print a sequence of {0} {1} numbers.", userNum, userTypeProper);
-            Even my_even = new Even();
-            Odd my_odd = new Odd();
-            my_even.Iterate(userNum);
-            my_odd.Iterate(userNum);
-            Console.WriteLine();
 
+            // Pulls the user input and converts to a number
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            // Checks to make sure that number is greater than 0
+            if (userNum < 1)
+            {
+                //  Tells user they are dumb and makes the userNum 5
+                Console.WriteLine("No, you have to pick a number greater than zero... Let's pretend you picked five..");
+                userNum = 5;
+                
+            }
+
+            //  Outputs the number of iterations and type that the user selected
+            Console.WriteLine("Here's your sequence of {0} {1} numbers:", userNum, userTypeProper);
+
+            //  Checks for the userType and instantiates and iterates accordingly
+            if (userTypeProper == "Even")
+            {
+                Even userInstance = new Even();
+                ReturnList = userInstance.Iterate(userNum);
+            }
+            else if (userTypeProper == "Odd")
+            {
+                Odd userInstance = new Odd();
+                ReturnList = userInstance.Iterate(userNum);
+            }
+            else if (userTypeProper == "Integer")
+            {
+                Integer userInstance = new Integer();
+                ReturnList = userInstance.Iterate(userNum);
+            }
+            else if (userTypeProper == "Prime")
+            {
+                Prime userInstance = new Prime();
+                ReturnList = userInstance.Iterate(userNum);
+            }
+            else if (userTypeProper == "Fibonacci")
+            {
+                Fibonacci userInstance = new Fibonacci();
+                ReturnList = userInstance.Iterate(userNum);
+            }
+
+
+            // Prints out the generated list for the user
+            ReturnList.ForEach(Console.WriteLine);
             Console.Read();
         }
     }
