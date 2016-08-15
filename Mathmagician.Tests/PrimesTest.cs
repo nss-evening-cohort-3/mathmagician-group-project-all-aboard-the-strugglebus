8f4766ea79747a7e224626fce1e6806b2a06cbe3
Andi DeVois <adevois@gmail.com>
2016-08-14 18:05:42 -0700

    primes

---
+++ Mathmagician.Tests/PrimesTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Mathmagician.Tests
+{
+    [TestClass]
+    public class PrimesTest
+    {
+        [TestMethod]
+        public void PrimesCreateAnInstance()
+        {
+            //Arrange
+
+            //Act
+            Primes testPrimes = new Primes();
+
+            //Assert
+            Assert.IsNotNull(testPrimes);
+        }
+
+        [TestMethod]
+        public void FirstPrime()
+        {
+            //Arrange
+            Primes myPrimes = new Primes();
+
+            //Act
+            int InitialPrime = myPrimes.StartofPrimes();
+
+            //Assert
+            Assert.AreEqual(2, InitialPrime);
+        }
+
+        [TestMethod]
+        public void NextPrime()
+        {
+            //Arrange
+            Primes myPrimes = new Primes();
+
+            //Act
+            int SecondPrime = myPrimes.AnotherPrime(3);
+
+            //Assert
+            Assert.AreEqual(3, SecondPrime);
+        }
+
+        [TestMethod]
+        public void PrimeList()
+        {
+            //Arrange
+            Primes myPrimes = new Primes();
+
+            //Act
+            List<int> PrimeNumbersList = myPrimes.ListPrimes(8);
+            List<int> ExpectedPrimes = new List<int> { 2, 3, 5, 7, 11, 13, 17, 19 };
+
+            //Assert
+            Assert.AreEqual(ExpectedPrimes.Count, PrimeNumbersList.Count);
+            CollectionAssert.AreEqual(PrimeNumbersList, ExpectedPrimes);
+        }
+    }
+}
---
+++ Mathmagician/Primes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class Primes
+    {
+        public int StartofPrimes()
+        {
+            return 2;
+        }
+
+        public int AnotherPrime(int v)
+        {
+            return 3;
+        }
+
+        public List<int> ListPrimes(int userNum)
+        {
+            List<int> PrimeSequence = new List<int>();
+            PrimeSequence.Add(2);
+            int nextPrimeNumber = 3;
+
+            while (PrimeSequence.Count < userNum)
+            {
+                int sqrt = (int)Math.Sqrt(nextPrimeNumber);
+                bool isPrime = true;
+                for (int i = 0; (int)PrimeSequence[i] <= sqrt; i++)
+                {
+                    if (nextPrimeNumber % PrimeSequence[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    PrimeSequence.Add(nextPrimeNumber);        
+                }
+
+                nextPrimeNumber += 2;
+            }
+
+            return PrimeSequence;
+        }
+    }
+}
